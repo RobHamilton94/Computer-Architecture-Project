b03c505442e956a459d9dc95b4777ac608480b9b
robhamilton94 <robhamilton94@live.co.uk>
2017-02-21 15:16:39 +0000

    Adds Task 3

---
+++ Task 3/User_Prompts.asm
@@ -0,0 +1,13 @@
+asect 0x00
+dc "Press 's' to save a string or 'r' to load one", 0x0a
+
+asect 0x30
+dc "Enter your string and press return to finish", 0x0a
+
+asect 0x60
+dc "Enter a 1 character filename", 0x0a
+
+asect 0x90
+dc "Enter filename of the string (return to exit)", 0x0a
+
+end
